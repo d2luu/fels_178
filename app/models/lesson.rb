5e0901269af4e6672b04234221ab424288e3472c
Dao Danh Luu <luubkk57@gmail.com>
2016-08-30 10:51:20 +0700

    init database

---
+++ app/models/lesson.rb
@@ -0,0 +1,7 @@
+class Lesson < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+
+  has_many :results, dependent: :destroy
+  has_many :words, through: :results
+end
---
+++ app/models/word.rb
@@ -0,0 +1,6 @@
+class Word < ApplicationRecord
+  belongs_to :category
+
+  has_many :answers, dependent: :destroy
+  has_many :lessons, through: :results
+end
