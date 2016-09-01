5e497761ecc05545d42c76995ea90c27ed720e1f
Dao Danh Luu <luubkk57@gmail.com>
2016-09-01 10:22:00 +0700

    admin edit, delete category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   has_many :words, dependent: :destroy
   has_many :lessons, dependent: :destroy
+
+  validates :name, presence: :true
+  validates :description, presence: :true
 end
